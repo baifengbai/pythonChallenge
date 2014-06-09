f26df02eb7c8d358cd585798918716884ae21f3c
Vipul <vipul.sharma20@gmail.com>
2014-06-09 12:52:07 +0530

    challenge6 description

--- pythonChallenge6.py
+++ pythonChallenge6.py
@@ -1,3 +1,8 @@
+"""
+the page consist of a paypal donation button which redirects to paypal.. the comment after <html> says "zip" ... changing the url has /pc/def/zip.html says "Yes. Find the zip." that means we have to find some zip file !! :D ... go and change url as "/def/channle.zip" (channel is from the previous url)
+you will get a zip file.. extract it and read the read me and run this script .. (some changes will be required) 
+"""
+
 t = open('90052.txt', 'r')
 name = '90052.txt'
 import re
